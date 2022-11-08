fcd42173e18d42ca6353c63ddd12abaee5376266
Shah Nawaj Fahim <shahnawasfahim@gmail.com>
2022-11-09 04:19:31 +0600

    mongo

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m7mohq7.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('broUser').collection('services');
+        // const reviewCollection = client.db('BroChef').collection('reviews');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
+        app.get('/allservices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
-
-    res.send('server is running')
+    res.send('Chef server is running')
 })
-
 app.listen(port, () => {
-    console.log(`server is running on ${port}`);
+    console.log(`Chef server running on ${port}`);
 })
+
+
+
+// require('crypto').randomBytes(64).toString('hex')
